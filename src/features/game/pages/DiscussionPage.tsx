c0fe772bd3f489018239aeb69f4d628cee8029e3
Jam Silvestre <jamsilvestre@Jams-MacBook-Pro-2026.local>
2026-06-17 00:43:24 +0800

    chore: check deployment test 1

--- src/features/game/pages/DiscussionPage.tsx
+++ src/features/game/pages/DiscussionPage.tsx
@@ -120,6 +120,7 @@ export function DiscussionPage() {
   ])
 
   if (isRoomLoading) {
+    console.log('check')
     return (
       <PageShell compact>
         <Loader fullPage label="Loading room" />
